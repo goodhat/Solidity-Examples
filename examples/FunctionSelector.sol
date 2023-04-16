28c3e7428e44ea3c77690b1be42dddf32f0cb324
goodhat <goodhatchou@gmail.com>
2023-04-16 17:56:39 +0800

    week 6

---
+++ examples/FunctionSelector.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract FunctionSelector {
+    /*
+    "transfer(address,uint256)"
+    0xa9059cbb
+    "transferFrom(address,address,uint256)"
+    0x23b872dd
+    */
+    function getSelector(string calldata _func) external pure returns (bytes4) {
+        return bytes4(keccak256(bytes(_func)));
+    }
+    // We can use encode the selector with data to make a call.
+    // addr.call(abi.encodeWithSignature("transfer(address,uint256)", 0xSomeAddress, 123))
+}
